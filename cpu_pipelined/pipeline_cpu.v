ac849f85c927ba446639a466ac9156e4f1fa2367
AkshanshK04 <akshanshk0406@gmail.com>
2026-04-05 09:03:56 +0530

    reg file signals passing done

--- cpu_pipelined/pipeline_cpu.v
+++ cpu_pipelined/pipeline_cpu.v
@@ -1,19 +1,60 @@
+/* 
+action : 6 stage pipelined 16 bit cpu
+*/
+
+`timescale 1ns/1ns
+
 module pipeline_cpu (
 
-    input clk,
-    input reset,
-    input [15:0] ext_data,
-    output [15:0] result 
+    input wire clk,
+    input  wire rst_n,
+
+    /* mem mapped sensor port */
+    input wire [15:0] sensor_data,
+    input wire        sensor_valid,
+
+    /* interrupt interface */
+    input wire   irq,
+    input wire [15:0]  irq_vector,
+    output reg         irq_ack,
+
+    /* alert ops ( mem mapped io at 0xF0-0xF2 ) */
+    output reg    alert_buzzer,
+    output reg    alert_bt,
+    output reg    alert_wifi,
+
+
+    /* debug */
+
+    output wire [15:0]  dbg_pc,
+    output wire [15:0] dbg_alu_out ,
+    output wire        dbg_halted
 );
 
-    reg [15:0] pc;
-    wire [15:0] instr;
 
-    reg[15:0] if_id_instr;
-    reg[15:0] id_ex1, id_exb;
-    reg[3:0] id_ex_opcode;
-    reg[15:0] ex_mem_out;
-    reg[15:0] mem_wb_out ;
+
+    /* initialize */
+
+
+    /* reg file signals first */
+    reg [2:0]   rf_rs1, rf_rs2, rf_rd_w;
+    reg [15:0]  rf_wdata;
+    reg         rf_we;
+    wire [15:0] rf_rdata1, rf_data2;
+
+
+
+    reg_file u_rf (
+
+        .clk (clk),
+        .we (rf_we),
+        .rs1 (rf_rs1),
+        .rs2  (rf_rs2) ,
+        .rd  (rf_rd_w ),
+        .wdata  (rf_wdata) ,
+        .rdata1 (rf_rdata1) ,
+        .rdata2 (rf_rdata2)
+    );
 
     instruction_memory imem( pc, instr );
     register_file rf(clk, if_id_instr[11:8], if_id_instr[7:4], if_id_instr[3:0], mem_wb_out, 1, id_exa , id_exb);
